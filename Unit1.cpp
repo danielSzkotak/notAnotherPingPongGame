bfeca7479241b4d067f3dbf5b4306e296aab7e4b
danielSzkotak <daniel.szkotak.programista@gmail.com>
2021-10-05 20:33:43 +0200

    Added mouve around hero feature

--- Unit1.cpp
+++ Unit1.cpp
@@ -14,4 +14,44 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 {
 }
 //---------------------------------------------------------------------------
- 
+
+void __fastcall TForm1::heroTimerLeftTimer(TObject *Sender)
+{
+        hero->Left -=10;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::heroTimerRightTimer(TObject *Sender)
+{
+        hero->Left += 10;        
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::FormKeyDown(TObject *Sender, WORD &Key,
+      TShiftState Shift)
+{
+        if (Key == VK_LEFT) heroTimerLeft->Enabled = true;
+        if (Key == VK_RIGHT) heroTimerRight->Enabled = true;
+        if (Key == VK_UP) heroTimerUp->Enabled = true;
+        if (Key == VK_DOWN) heroTimerDown->Enabled = true;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::FormKeyUp(TObject *Sender, WORD &Key,
+      TShiftState Shift)
+{
+        if (Key == VK_LEFT) heroTimerLeft->Enabled = false;
+        if (Key == VK_RIGHT) heroTimerRight->Enabled = false;
+        if (Key == VK_UP) heroTimerUp->Enabled = false;
+        if (Key == VK_DOWN) heroTimerDown->Enabled = false;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::heroTimerUpTimer(TObject *Sender)
+{
+       hero->Top -=10;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::heroTimerDownTimer(TObject *Sender)
+{
+       hero->Top +=10;
+}
+//---------------------------------------------------------------------------
